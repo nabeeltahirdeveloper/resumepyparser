b8edbb2da48c723232b196f2604a0875fcdaa80c
nabeeltahir <nabeeltahirdeveloper@gmail.com>
2023-04-28 15:55:02 +0500

    add

---
+++ dataSample.js
@@ -0,0 +1,354 @@
+export const sampleData = {
+  data: {
+    achievements: {
+      heading: "Achievements",
+      index: 9,
+      items: [
+        {
+          Description: "",
+        },
+      ],
+      visible: false,
+    },
+    associations: {
+      heading: "Associations",
+      index: 7,
+      items: [  
+        {
+          Name: "",
+          Role: [
+            {
+              Name: "",
+            },
+          ],
+        },
+      ],
+      visible: false,
+    },
+    additional_skills: {
+      heading: 'Additional Skills',
+      index: 17,
+      items: [
+        {
+          Name: '',
+          Role: [
+            {
+              Name: '',
+            },
+          ],
+        },
+      ],
+      visible: false,
+    },
+    awards: {
+      enable: false,
+      heading: "Honors & Awards",
+      items: [],
+      visible: false,
+    },
+    certifications: {
+      heading: "Certifications",
+      index: 3,
+      items: [
+        {
+          date: "",
+          id: "",
+          issuer: "",
+          location: "",
+          summary: "",
+          title: "",
+        },
+      ],
+      visible: true,
+    },
+    courses: {
+      heading: "Courses",
+      index: 6,
+      items: [
+        {
+          date: "",
+          id: "",
+          issuer: "",
+          location: "",
+          summary: "",
+          title: "",
+        },
+      ],
+      visible: false,
+    },
+    education: {
+      enable: true,
+      heading: "Education",
+      index: 2,
+      items: [
+        {
+          awards: "",
+          courseworks: "",
+          degree: "",
+          description: "",
+          enabled: true,
+          end: "",
+          endDate: "",
+          extracurricular: "",
+          field: "",
+          gpa: "",
+          grade: "",
+          id: "",
+          institution: "",
+          location: "",
+          major: "",
+          month: "",
+          start: null,
+          startDate: null,
+          summary: "",
+          title: "",
+          year: "",
+        },
+      ],
+      visible: true,
+    },
+    expertise: {
+      heading: "Areas of Expertise include:",
+      items: [],
+      visible: false,
+    },
+    extras: {
+      enable: false,
+      heading: "Additional Information",
+      items: [],
+    },
+    hobbies: {
+      enable: false,
+      heading: "Hobbies",
+      items: [],
+      visible: false,
+    },
+    languages: {
+      enable: false,
+      heading: "Languages",
+      items: [],
+    },
+    military: {
+      heading: "Military",
+      index: 15,
+      items: [
+        {
+          RankAchieved: {
+            CurrentOrEndRank: "",
+          },
+          DatesOfService: {
+            StartDate: {
+              YearMonth: "",
+            },
+            EndDate: {
+              YearMonth: "",
+            },
+          },
+          "branch": "",
+        },
+      ],
+      visible: false,
+    },
+    objective: {
+      body: "",
+      enable: true,
+      heading: "Summary",
+      index: 5,
+      visible: true,
+    },
+    custom: {
+      body: "",
+      enable: false,
+      heading: "Custom",
+      title:"",
+      index: 16,
+      visible: false,
+    },
+    patents: {
+      heading: "Patents",
+      index: 11,
+      items: [
+        {
+          Description: "",
+          Inventors: {
+            InventorName: [""],
+          },
+          PatentTitle: "",
+        },
+      ],
+      visible: false,
+    },
+    professionalAccomplishments: {
+      enable: false,
+      heading: "Professional Accomplishments",
+      items: [],
+      visible: false,
+    },
+    profile: {
+      address: {
+        city: "",
+        line1: "",
+        line2: "",
+        line3: "",
+        pincode: "",
+        state: "",
+        zipcode: "",
+      },
+      email: "",
+      firstName: "",
+      heading: "Profile",
+      index: 0,
+      lastName: "",
+      linkedinURL: "",
+      phone: "",
+      photo:
+        "https://pbs.twimg.com/profile_images/1197449172921847809/HhpxN86f_400x400.jpg",
+      photograph: "",
+      subtitle: "",
+      visible: true,
+      website: "",
+    },
+    projects: {
+      heading: "Projects",
+      index: 10,
+      items: [
+        {
+          Title: "",
+          Company: "",
+          Duration: "",
+          TechnologiesUsed: "",
+          Summary: "",
+        },
+      ],
+      visible: false,
+    },
+    publications: {
+      heading: "Publications",
+      index: 12,
+      items: [
+        {
+          Title: "",
+          JournalOrSerialName: "",
+          Issue: "",
+        },
+      ],
+      visible: false,
+    },
+    references: {
+      enable: false,
+      heading: "References",
+      items: [],
+    },
+    security_creds: {
+      heading: "Security Credentials",
+      index: 14,
+      items: [
+        {
+          EffectiveDate: {
+            FirstIssuedDate: {
+              Year: "",
+            },
+          },
+          Name: "",
+        },
+      ],
+      visible: false,
+    },
+    skills: {
+      enable: true,
+      heading: "Skills",
+      index: 4,
+      items: [],
+      visible: true,
+    },
+    // additional_skills: {
+    //   heading: "Additonal Skills",
+    //   index: 16,
+    //   items: [],
+    //   visible: false,
+    // },
+    social: {
+      enable: false,
+      heading: "Social",
+      items: [],
+      visible: false,
+    },
+    speaking_engagements: {
+      heading: "Speaking Engagements",
+      index: 13,
+      items: [
+        {
+          Description: "",
+          EventName: "",
+          EventType: "",
+        },
+      ],
+      visible: false,
+    },
+    training: {
+      heading: "Training",
+      index: 8,
+      items: [
+        {
+          "Description": "",
+          "EndDate": {
+            Year: "",
+          },
+          "Entity": null,
+          "Qualifications": {
+            "Qualification": [],
+          },
+          "TrainingName": null,
+          "Type": "",
+        },
+      ],
+      visible: false,
+    },
+    volunteerExperience: {
+      enable: false,
+      heading: "Volunteer Experience",
+      items: [],
+      visible: false,
+    },
+    work: {
+      enable: true,
+      heading: "Work Experience",
+      index: 1,
+      items: [
+        {
+          company: "",
+          description: "",
+          enabled: true,
+          end: "",
+          endDate: "",
+          endMonth: "",
+          endYear: "",
+          id: "",
+          location: "",
+          position: "",
+          role: "",
+          start: "",
+          startDate: "",
+          startMonth: "",
+          startYear: "",
+          summary: '',
+          title: "",
+          website: "",
+        },
+      ],
+      visible: true,
+    },
+  },
+  oid: "5fb681c7c8d2ca429821d3d2",
+  theme: {
+    colors: {
+      accent: "#f44336",
+      background: "#ffffff",
+      primary: "#212121",
+    },
+    font: {
+      family: "Montserrat",
+    },
+    layout: "castform",
+  },
+};
