72037455d6e0c7af3f4a00239ec1a9bc3bf93d26
nabeeltahir <nabeeltahirdeveloper@gmail.com>
2023-04-28 13:13:03 +0500

    fix

--- mainTestTwo.py
+++ mainTestTwo.py
@@ -1,11 +1,57 @@
-import sys, fitz
+import openai
+from application.webapp.nlp.main import convertFileToText
+import json
 
-def convertToText(fname):
-    doc = fitz.open(fname)
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    return text
+import time
+from pyresparser import ResumeParser
+start_time = time.time()
 
-if __name__ == "__main__":
-    print(convertToText("resume.pdf"))
+
+
+def chat(text):
+    global messages
+    messages.append({"role": "user", "content": text})
+    return openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages,
+      max_tokens= 2000
+    )
+
+
+extractedText = convertFileToText("OmkarResume.pdf")
+finalText = f"""
+         create a json parsing from below text \n {extractedText}
+         """
+messages = []
+
+answer = chat(finalText)
+
+# save json file
+
+with open('OmkarResume-gpt.json', 'w') as outfile:
+    json.dump(json.loads(answer['choices'][0]['message']['content']), outfile)
+
+
+end_time = time.time()
+
+execution_time = end_time - start_time
+
+data = ResumeParser('OmkarResume.pdf', skills_file='skills.csv').get_extracted_data()
+
+with open('OmkarResume-parser.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+print("Execution time:", execution_time)
+
+
+# if __name__ == "__main__":
+#     messages = []
+#     while True:s
+#         text = input("You: ")
+#         finalText = f"""
+#         create a cover letter according to these parameters {text}
+#         max1000 limit"""
+#         # if text == "exit":
+#         #     break
+#         response = chat(finalText)
+#         print("AI: ", response['choices'][0]['message']['content'])
