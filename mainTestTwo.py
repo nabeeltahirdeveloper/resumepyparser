504a613e41acf06a5fbd7bd26474159ede6989fc
nabeeltahir <nabeeltahirdeveloper@gmail.com>
2023-04-28 14:57:38 +0500

    v1

--- mainTestTwo.py
+++ mainTestTwo.py
@@ -1,10 +1,97 @@
 import openai
-from application.webapp.nlp.main import convertFileToText
+# from application.webapp.nlp.main import convertFileToText
 import json
 
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser, PDFSyntaxError
+import io
+from pdfminer.converter import PDFPageAggregator, TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.layout import (
+    LAParams,
+    LTChar,
+    LTFigure,
+    LTTextBox,
+    LTTextLine,
+    LTTextLineHorizontal,
+)
 import time
 from pyresparser import ResumeParser
 start_time = time.time()
+
+
+def extract_text_from_pdf(pdf_path):
+    """
+    Helper function to extract the plain text from .pdf files
+    :param pdf_path: path to PDF file to be extracted (remote or local)
+    :return: iterator of string of extracted text
+    """
+    # https://www.blog.pythonlibrary.org/2018/05/03/exporting-data-from-pdfs-with-python/
+    if not isinstance(pdf_path, io.BytesIO):
+        # extract text from local pdf file
+        with open(pdf_path, "rb") as fh:
+            try:
+                for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+                    resource_manager = PDFResourceManager()
+                    fake_file_handle = io.StringIO()
+                    converter = TextConverter(
+                        resource_manager, fake_file_handle, laparams=LAParams()
+                    )
+                    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                    page_interpreter.process_page(page)
+
+                    text = fake_file_handle.getvalue()
+                    yield text
+
+                    # close open handles
+                    converter.close()
+                    fake_file_handle.close()
+            except PDFSyntaxError:
+                return
+    else:
+        # extract text from remote pdf file
+        try:
+            for page in PDFPage.get_pages(
+                pdf_path, caching=True, check_extractable=True
+            ):
+                resource_manager = PDFResourceManager()
+                fake_file_handle = io.StringIO()
+                converter = TextConverter(
+                    resource_manager, fake_file_handle, laparams=LAParams()
+                )
+                page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                page_interpreter.process_page(page)
+
+                text = fake_file_handle.getvalue()
+                yield text
+
+                # close open handles
+                converter.close()
+                fake_file_handle.close()
+        except PDFSyntaxError:
+            return
+
+
+
+
+def convertFileToText(fileName):
+    ext = fileName.split(".")[-1]
+    # print("file:", fileName)
+    if ext == "pdf":
+        # print("working as pdf")
+        text = ""
+        for page in extract_text_from_pdf(fileName):
+            text += " " + page
+        return text
+    elif ext == "docx":
+        text = getTextFromDocx(fileName)
+        return text
+    elif ext == "doc":
+        text = getTextFromDocx(fileName)
+        return text
+    else:
+        None
 
 
 
